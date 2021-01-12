956ef04607353a2b4f1e56d9328635243ca70e72
Micael Austad <micael.austad@gmail.com>
2021-01-12 20:15:54 +0100

    changed to reddit instead of ntnu for dump

--- util/1/ntnu_jobs/lol.py
+++ util/1/ntnu_jobs/lol.py
@@ -1,5 +1,3 @@
 import requests
-import json
-with open("dump.json", "w") as file:
-    file.write(json.dumps(requests.get(
-        "https://www.ntnu.edu/vacancies?p_p_id=vacanciesportlet_WAR_vacanciesportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=feed&p_p_cacheability=cacheLevelPage").json()))
+with open("dump.html", "wb") as file:
+    file.write(requests.get('https://old.reddit.com/').content)
